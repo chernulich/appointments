6736a22c5fcc3dd2b50b27451d0e10ef31cfc2ac
pvitkovsky <vitkovsky1337@gmail.com>
2018-07-26 13:29:39 +0300

    Biweekly test

--- 2018-07-25_ical_ii/src/pg/ical/CalendarTest.java
+++ 2018-07-25_ical_ii/src/pg/ical/CalendarTest.java
@@ -1,28 +1,14 @@
 package pg.ical;
-import java.net.SocketException;
+import java.util.Date;
 
-import net.fortuna.ical4j.model.Date;
-import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.util.FixedUidGenerator;
-import net.fortuna.ical4j.util.UidGenerator;
-
+import biweekly.component.VEvent;
 
 public class CalendarTest {
-	public static void main(String[] args) throws SocketException {
-		java.util.Calendar calendar = java.util.Calendar.getInstance();
-		calendar.set(java.util.Calendar.MONTH, java.util.Calendar.DECEMBER);
-		calendar.set(java.util.Calendar.DAY_OF_MONTH, 25);
-
-		// initialise as an all-day event..
-		VEvent christmas = new VEvent(new Date(calendar.getTime()), "Christmas Day");
-
-		// Generate a UID for the event..
-		UidGenerator ug = new FixedUidGenerator("1");
-		christmas.getProperties().add(ug.generateUid());
-
-		net.fortuna.ical4j.model.Calendar cal = new net.fortuna.ical4j.model.Calendar();
-		cal.getComponents().add(christmas);
-		System.out.println(cal.toString());
+	public static void main(String[] args) {
+		VEvent event = new VEvent();
+		Date start = new Date();
+		event.setDateStart(start, false);
+		System.out.println(event.toString());
 	 }
 }
 	
