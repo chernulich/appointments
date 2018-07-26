a9c5a1a5333dfbce1a4d24b97cd2f4093e86f76a
pvitkovsky <vitkovsky1337@gmail.com>
2018-07-26 17:16:33 +0300

    proof-of-concepts

---
+++ 2018-07-25_ical_ii/src/appointer/component/AppUser.java
@@ -0,0 +1,25 @@
+package appointer.component;
+
+import appointer.User;
+
+/**
+ * mock User class
+ */
+public class AppUser implements User {
+
+	private String name;
+	
+	public AppUser(String name) {
+		this.name = name;
+	}
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
